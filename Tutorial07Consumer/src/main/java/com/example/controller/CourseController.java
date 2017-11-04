c2cba48e8e9e0700a4c840ac375d1f378c0b38ef
muthianbl <muthianbl@gmail.com>
2017-11-04 19:18:24 +0700

    consumer

---
+++ Tutorial07Consumer/src/main/java/com/example/controller/CourseController.java
@@ -0,0 +1,43 @@
+package com.example.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.model.CourseModel;
+import com.example.service.CourseService;
+
+@Controller
+public class CourseController
+{
+    @Autowired
+    CourseService courseDAO;
+    
+    //Latihan Tutorial 5 nomor 2. Menampilkan data course.
+    @RequestMapping("/course/view/{id_course}")
+    public String viewCourse (Model model, @PathVariable(value = "id_course") String id)
+    {
+        CourseModel course = courseDAO.selectCourse(id);
+
+        if (course != null) {
+            model.addAttribute ("course", course);
+            return "view-course";
+        } else {
+            model.addAttribute ("id", id);
+            return "not-found-course";
+        }
+    }
+    
+    @RequestMapping("/course/viewall")
+    public String viewall (Model model)
+    {
+        List<CourseModel> courses = courseDAO.selectAllCourses ();
+        model.addAttribute ("courses", courses);
+
+        return "viewall-course";
+    }
+}
